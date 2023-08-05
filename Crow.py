d7e65e9d0a17397052308b65304f0554173405da
RRicky25 <rickyratnani25@gmail.com>
2023-08-05 10:50:53 +0530

    movements for crow done - the image scaling and tilting, kill using sprite group are remaining

---
+++ Crow.py
@@ -0,0 +1,74 @@
+import pygame
+from Net import *
+class Crow():
+    def __init__(self, x_start, y_start, x_crop, y_crop, x_end, y_end):
+        self.x_start,self.y_start = x_start,y_start
+        self.x_crop,self.y_crop = x_crop,y_crop
+        self.x_end,self.y_end = x_end,y_end
+        self.x, self.y = x_start, y_start
+
+        self.direction1 = 1
+        if self.x_crop >= self.x: 
+            self.direction1 = 1
+        else:
+            self.direction1 = -1
+
+        self.direction2 = 1 
+        if self.x_crop >= self.x_end: self.direction2 = -1
+        else: self.direction2 = 1
+
+        # slope 1 and 2 will be decided after phase 0 and phase 2 respectively
+        self.slope1 = 0
+        self.slope2 = 0
+
+        self.velocity = 1
+        self.rest_timer = 0
+        self.see_Distance = 20
+        self.current_frame = 0
+
+        self.phase = 0
+
+        #loading the images
+        image_filenames = ['crow1.png', 'crow2.png', 'crow3.png', 'crow4.png', 'crow5.png', 'crow6.png', 'crow7.png']
+        self.images = [pygame.image.load("./assets/images/crow/" + filename) for filename in image_filenames]
+        for i in range(len(self.images)):
+            self.images[i] = pygame.transform.scale(self.images[i], (16 * 5, 16 * 5))
+
+
+    def draw(self, win):
+        self.move()
+        crow_rect = self.images[self.current_frame].get_rect(topleft = (self.x, self.y))
+        win.blit(self.images[self.current_frame], crow_rect)
+        self.current_frame += 1
+        if self.current_frame == 7:
+            self.current_frame = 0
+
+
+    def move(self):
+        if self.phase == 0:
+            if abs(self.x_crop - self.x) < self.see_Distance:
+                self.phase = 1
+                self.slope1 = (self.y_crop - self.y) / (self.x_crop - self.x)
+            else:
+                self.x += self.direction1 * self.velocity
+            
+        elif self.phase == 1:
+            if abs(self.x_crop - self.x) < 2 and abs(self.y_crop - self.y) < 2:
+                self.phase = 2
+            else:
+                self.x += self.direction1 * self.velocity
+                self.y += self.slope1 * self.direction1 * self.velocity
+
+        elif self.phase == 2:
+            if self.rest_timer >= 120:
+                self.phase = 3
+                self.slope2 = (self.y_end - self.y) / (self.x_end - self.x)
+            else:
+                self.rest_timer += 1
+        
+        else:
+            if abs(self.y_end - self.y) < 2:
+                self.y = self.y
+            else:
+                self.y += self.slope2 * self.direction2 * self.velocity
+            self.x += self.direction2 * self.velocity
